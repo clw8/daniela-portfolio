b7bf46e8fcce87d74f3639803eee062c3763e055
Chris Walsh <chris.w894j@gmail.com>
2018-10-13 01:59:24 +0200

    contact form

---
+++ js/main.js
@@ -0,0 +1,32 @@
+//contact form
+const contactSubmit = document.querySelector('#contact-submit');
+const formInputs = document.querySelectorAll('.form-input');
+contactSubmit.addEventListener('click', (e)=> {
+    e.preventDefault();
+    //json triggers preflight, so we are manually serializing the input data rather than sending as a json object
+   let data = "";
+    for (let i = 0; i < formInputs.length; ++i) {
+      let input = formInputs[i];
+      if (input.name) {
+      data += `${input.value}%$`;
+      }
+    }
+
+    let xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+        document.getElementById("submit-res").innerHTML = "Thanks for getting in touch. I'll get back to you soon :)";
+        document.getElementById("submit-res").classList.add('ok');
+      }
+      else if(this.status !== 0 && this.status !== 200 && this.status !== undefined){
+        document.getElementById("submit-res").innerHTML = "Something went wrong..Please try again.";       
+        document.getElementById("submit-res").classList.add('error');
+      }
+    };
+
+   const url = "https://script.google.com/macros/s/AKfycbyuFIEsLSei7wgbmO4aUuurv_5xAypuyqweNQYrdZNY01pQ7vg/exec";
+   xhttp.open("POST", url, true);
+    xhttp.setRequestHeader('Content-Type', 'text/plain');
+   xhttp.setRequestHeader('Accept', 'text/plain');
+    xhttp.send(data);
+})
